fb7603fc11b5ffede790eccaca35c73b4596229d
Ramiz Alasgarov <ramiz90151@gmail.com>
2024-05-26 21:15:07 +0200

    sout ok3

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -4,5 +4,6 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("ok");
         System.out.println("ok2");
+        System.out.println("ok3");
     }
 }
